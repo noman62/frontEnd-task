749d0aeb2593a1cd69bcfab95b79e32c3eec4919
Asadullah Al Noman <76659134+noman62@users.noreply.github.com>
2024-04-29 21:20:17 +0600

    Final Commit

--- src/components/ProductsData/ProductsData.jsx
+++ src/components/ProductsData/ProductsData.jsx
@@ -1,58 +1,58 @@
 const ProductsData = [
   {
     id:1,
-    name: "Stylish Men's Shirt",
+    name: "Fizza",
     price: 40,
     Image:
-      'https://images.unsplash.com/photo-1603252109303-2751441dd157?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZvb2R8ZW58MHx8MHx8fDA%3D',
     selectedQuantity: 0
   },
   {
     id:2,
-    name: "Classic Men's Watch",
+    name: "Montréal",
     price: 80,
     Image:
-      'https://images.unsplash.com/photo-1691415993286-91f1586a5e20?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://images.unsplash.com/photo-1496116218417-1a781b1c416c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     selectedQuantity: 0
   },
   {
     id:3,
-    name: "Leather Men's Shoes",
+    name: "Cracker",
     price: 60,
     Image:
-      'https://images.unsplash.com/photo-1549298916-b41d501d3772?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D',
     selectedQuantity: 0
   },
   {
     id:4,
-    name: "Elegant Women's Dress",
+    name: "Vegetable",
     price: 50,
     Image:
-      'https://plus.unsplash.com/premium_photo-1664790560422-3b8eeb1d72a0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHdvbWVuJTIwZHJlc3N8ZW58MHx8MHx8fDA%3D',
+      'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     selectedQuantity: 0
   },
   {
     id:6,
-    name: "Stylish Women's Heels",
+    name: "Burger",
     price: 55,
     Image:
-      'https://images.unsplash.com/photo-1535043934128-cf0b28d52f95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2VyfGVufDB8fDB8fHww',
     selectedQuantity: 0
   },
   {
     id:9,
-    name: 'Wireless Headphones',
+    name: 'Burger',
     price: 120,
     Image:
-      'https://images.unsplash.com/photo-1609081219090-a6d81d3085bf?q=80&w=1926&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YnVyZ2VyfGVufDB8fDB8fHww',
     selectedQuantity: 0
   },
   {
     id:10,
-    name: 'Wireless Headphones',
+    name: 'Eggs',
     price: 120,
     Image:
-      'https://images.unsplash.com/photo-1491927570842-0261e477d937?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://images.unsplash.com/photo-1582722872445-44dc5f7e3c8f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZWdnfGVufDB8fDB8fHww',
     selectedQuantity: 0
   }
 ]
